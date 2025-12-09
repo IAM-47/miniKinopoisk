f022c25d0565a2b984175204e53016c79df29151
IAM <sirotinroman18@gmail.com>
2025-12-09 17:35:56 +0300

    third?

---
+++ pkg/config/config.go
@@ -0,0 +1,37 @@
+package config
+
+import (
+	"log"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	Server   ServerConfig   `mapstructure:"server"`
+	Database DatabaseConfig `mapstructure:"database"`
+}
+
+type ServerConfig struct {
+	Port string `mapstructure:"port"`
+}
+
+type DatabaseConfig struct {
+	DSN string `mapstructure:"dsn"`
+}
+
+func Load() *Config {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	viper.SetConfigType("yaml")
+
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatalf("Error reading config file: %v", err)
+	}
+
+	var cfg Config
+	if err := viper.Unmarshal(&cfg); err != nil {
+		log.Fatalf("Unable to decode config: %v", err)
+	}
+
+	return &cfg
+}
